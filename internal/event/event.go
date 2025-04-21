8f60f857054fdf5fe7cc0b4c9f95777e6f77dc34
H33Kz <qlmejk@gmail.com>
2025-04-21 18:18:55 +0200

    Added basic event architecture components and first iteration of sequence event

---
+++ internal/event/event.go
@@ -0,0 +1,34 @@
+package event
+
+type Event struct {
+	Type string
+	Data interface{}
+}
+
+type SequenceEvent struct {
+	Label      string
+	DeviceName string
+	Function   string
+	Site       int
+	Timeout    int
+}
+
+type EventBus struct {
+	subscribers map[string][]chan<- Event
+}
+
+func NewEventBus() *EventBus {
+	return &EventBus{
+		subscribers: make(map[string][]chan<- Event),
+	}
+}
+
+func (eBus *EventBus) Subscribe(eventType string, eventChan chan<- Event) {
+	eBus.subscribers[eventType] = append(eBus.subscribers[eventType], eventChan)
+}
+
+func (eBus *EventBus) Publish(event Event) {
+	for _, subscriber := range eBus.subscribers[event.Type] {
+		subscriber <- event
+	}
+}
