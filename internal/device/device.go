3221df0ebcf0da71e5cdc4119cc2f71c16c79e9e
H33Kz <qlmejk@gmail.com>
2025-04-21 18:20:21 +0200

    Device interface update: methods to handle events and resolve incoming messages

--- internal/device/device.go
+++ internal/device/device.go
@@ -1,6 +1,13 @@
 package device
 
+import (
+	"checkerbox/internal/event"
+	"checkerbox/internal/test"
+)
+
 type Device interface {
+	SequenceEventHandler(chan test.Result)
 	FunctionResolver()
+	GetEventChannel() chan event.Event
 	Print()
 }
