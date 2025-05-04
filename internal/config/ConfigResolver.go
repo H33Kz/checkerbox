b349043400fab17d192509f7da6fc438cb2ac8cc
H33Kz <qlmejk@gmail.com>
2025-05-04 19:24:27 +0200

    Ad. prev. change

--- internal/config/ConfigResolver.go
+++ internal/config/ConfigResolver.go
@@ -9,6 +9,8 @@ func DeviceEntryResolver(deviceEntry map[string]string) (device.Device, []error)
 	switch deviceEntry["device"] {
 	case "genericuart":
 		return device.NewGenericUart(deviceEntry)
+	case "testdevice":
+		return device.NewTestDevice(deviceEntry)
 	default:
 		var errorTable []error
 		errorTable = append(errorTable, errors.New("Specified device not supported: "+deviceEntry["device"]))
