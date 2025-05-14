6d71edd32f99df053aa71cd99fe40bb2660db2f2
H33Kz <qlmejk@gmail.com>
2025-05-14 20:46:07 +0200

    full config loading overhauled
    Configuration for devices and sequence steps change so that things that
    can be statically loaded are. Settings related to inner workings of a
    test moved to device side. Main loop doesn't work with dynamically
    loaded settings

--- internal/config/AppConfig.go
+++ internal/config/AppConfig.go
@@ -13,6 +13,25 @@ type AppSettings struct {
 	Uiengine string `yaml:"uiengine"`
 }
 
+type DeviceSettings struct {
+	Site       int            `yaml:"site"`
+	DeviceName string         `yaml:"device_name"`
+	Settings   map[string]any `yaml:"settings"`
+}
+
+type SequenceStepSettings struct {
+	StepLabel    string         `yaml:"step_label"`
+	Retry        int            `yaml:"retry"`
+	Device       string         `yaml:"device"`
+	Timeout      int            `yaml:"timeout"`
+	StepSettings map[string]any `yaml:"stepsettings"`
+}
+
+type Config struct {
+	Hardware []DeviceSettings       `yaml:"hardware"`
+	Sequence []SequenceStepSettings `yaml:"sequence"`
+}
+
 func NewAppSettings() *AppSettings {
 	appSettingsFile, err := os.ReadFile("app.yml")
 	if err != nil {
@@ -24,4 +43,25 @@ func NewAppSettings() *AppSettings {
 		log.Fatal(err.Error())
 	}
 	return &appSettingsInstance
+}
+
+func NewConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var unmarshalledData Config
+	err = yaml.Unmarshal(file, &unmarshalledData)
+	if err != nil {
+		return nil, err
+	}
+	return &unmarshalledData, nil
+}
+
+func (c *Config) GetSequenceConfig() []SequenceStepSettings {
+	return c.Sequence
+}
+
+func (c *Config) GetHardwareConfig() []DeviceSettings {
+	return c.Hardware
 }
